df23904a24d368054cf65319609e2f32d70401bf
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-03-23 04:21:30 -0400

    commit tree ea2cc85396530fd40d275c209161387b31e75e85

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchCats} from "./catsSlice";
+import CatList from "./CatList";
 
 function Cats() {
+
+  const catPics = useSelector((state) => state.entities)
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.entities.status);
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, [dispatch]);
+
+  console.log(catPics);
+
   return (
     <div>
       <h1>CatBook</h1>
       {/* add CatList component here */}
+      <CatList catPics={catPics}/>
     </div>
   );
 }
--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,41 @@
-export function fetchCats() {}
+//action createors
+export function fetchCats(cats) {
 
-export default function catsReducer() {}
+    //dispatch initial action to set up as loading state
+    return function (dispatch){
+    dispatch({type: "cats/catsLoading"});
+
+    //initiate network request with fetch
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then((r) => r.json())
+        .then((cats) => 
+            dispatch({
+                type: "cats/catsLoaded",
+                payload: cats.images,
+            })
+        );
+    };
+}
+
+const initialState = {
+    entities: [], //array of cats
+    status: "idle", //loading state
+}
+
+export default function catsReducer(state = initialState, action) {
+    switch(action.type){
+        case "cats/catsLoading":
+            return{
+                ...state,
+                status: "loading",
+            };
+        case "cats/catsLoaded":
+            return{
+                ...state,
+                entities: action.payload,
+                status: "idle",
+            };
+        default:
+            return state;
+    }
+}
